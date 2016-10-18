920be515e3c4b7d3bc33b4c9564a7fdd2c3f9313
mzamoun <mourad.zamoun@gmail.com>
2016-10-18 12:20:06 +0200

    completer pkg

--- maven_add_project_quick.sh
+++ maven_add_project_quick.sh
@@ -1,8 +1,8 @@
 
 Usage="
-	$0 pkg appName 
+	$0 pkg appName [deleteSrcJava=Y]
 ex:
-	$0 com.sql spring_boot_jms
+	$0 com.sql spring_boot_jms N
 "
 
 if (($#<2)); then echo "$Usage"; exit 1; fi
@@ -10,7 +10,17 @@ if (($#<2)); then echo "$Usage"; exit 1; fi
 pkg=$1
 appName=$2
 
-mvn archetype:generate -DgroupId=$pkg -DartifactId=$appName -DarchetypeArtifactId=maven-archetype-quickstart -Dversion=0.1 -DinteractiveMode=false
+deleteSrcJava=$3
+
+mvn archetype:generate -DgroupId=$pkg.$appName -DartifactId=$appName -DarchetypeArtifactId=maven-archetype-quickstart -Dversion=0.1 -DinteractiveMode=false
+
+mkdir $appName/src/main/resources
+mkdir $appName/src/test/resources
+
+if [ "$deleteSrcJava" == "Y" -o "$deleteSrcJava" == "" ]; then
+	find $appName/src/main/java -name "*.java" -exec rm -f {} \;
+	find $appName/src/test/java -name "*.java" -exec rm -f {} \;
+fi
 
 ls -R $appName
 
